ecf7c6b7bd53e64ef64ab7df352bae49f820ca51
Lara <lara.salman01@hotmail.com>
2025-04-10 00:04:54 +0200

    add animation to home page

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,27 @@ function App() {
       >
         <div className="flex flex-col gap-1">
           <h1 className="text-5xl font-bold text-shade-100">
-           Hi I'm <span className="text-primary">Lara,</span>
+            Hi I'm <span className="text-primary">Lara,</span>
           </h1>
-          
-        </div>
 
+          <div className="flex flex-row gap-2 items-center">
+            <p className="text-3xl font-bold text-shade-100">
+              A{" "}
+              <span className="text-primary animate-typewriter">
+                <TypeAnimation
+                  sequence={[
+                    "Fullstack Developer", 1000,
+                    "Frontend Developer", 1000,
+                    "Backend Developer", 1000,
+                  ]}
+                  wrapper="span"
+                  speed={30}
+                  repeat={Infinity}
+                />
+              </span>
+            </p>
+          </div>
+        </div>
 
         <p className="text-base font-medium text-shade-100">
           I’m an aspiring Fullstack Developer graduating in 2026, with a strong
@@ -27,8 +43,8 @@ function App() {
         </p>
 
         <p className="text-base font-medium text-shade-100">
-          I’m currently seeking a LIA internship between November 2025 and April 2026,
-          where I can apply my skills, evolve and contribute to projects!
+          I’m currently seeking a LIA internship between November 2025 and April
+          2026, where I can apply my skills, evolve and contribute to projects!
         </p>
       </section>
 
