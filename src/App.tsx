2ea77825151434883dc835a2e0901cb6f0598c64
Lara <lara.salman01@hotmail.com>
2025-04-11 23:57:47 +0200

    create file for each section

--- src/App.tsx
+++ src/App.tsx
@@ -11,65 +11,6 @@ import "./index.css";
 function App() {
   return (
     <Layout>
-      <section
-        className="flex flex-col gap-2 bg-shade-800 min-h-screen py-28"
-        id="home"
-      >
-        <div className="flex flex-col gap-1">
-          <h1 className="text-5xl font-bold text-shade-100">
-            Hi I'm <span className="text-primary">Lara,</span>
-          </h1>
-
-          <div className="flex flex-row gap-2 items-center">
-            <p className="text-3xl font-bold text-shade-100">
-              A{" "}
-              <span className="text-primary animate-typewriter">
-                <TypeAnimation
-                  sequence={[
-                    "Fullstack Developer",
-                    1000,
-                    "Frontend Developer",
-                    1000,
-                    "Backend Developer",
-                    1000,
-                  ]}
-                  wrapper="span"
-                  speed={30}
-                  repeat={Infinity}
-                />
-              </span>
-            </p>
-          </div>
-        </div>
-
-        <p className="text-base font-medium text-shade-100">
-          I’m an aspiring Fullstack Developer graduating in 2026, with a strong
-          foundation in both front-end and back-end development.
-        </p>
-
-        <p className="text-base font-medium text-shade-100">
-          I’m currently seeking a LIA internship between November 2025 and April
-          2026, where I can apply my skills, evolve and contribute to projects!
-        </p>
-
-        <div className="flex flex-row gap-4">
-          <Social
-            link="https://www.linkedin.com/in/lara-salman-00224b1b2/"
-            ariaLabel="LinkedIn profile"
-          >
-            <Icon icon="ri:linkedin-fill" className="size-6" />
-          </Social>
-
-          <Social link="https://github.com/Laraass" ariaLabel="GitHub profile">
-            <Icon icon="mdi:github" className="size-6" />
-          </Social>
-
-          <Social link="" ariaLabel="My CV (Opens in new tab)">
-            <Icon icon="tabler:file-cv-filled" className="size-6" />
-          </Social>
-        </div>
-      </section>
-
       <section className="bg-shade-700 w-full min-h-screen py-28" id="about-me">
         {/* About Me */}
       </section>
