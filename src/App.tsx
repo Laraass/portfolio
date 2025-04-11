06fe7ea504bd4e6e9c01ee937312027e99aa7397
Lara <lara.salman01@hotmail.com>
2025-04-11 23:42:12 +0200

    Add socials to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Layout from "./components/Layout";
 import Header from "./components/Header";
+import Social from "./components/Social";
+import { Icon } from "@iconify/react";
 import { TypeAnimation } from "react-type-animation";
 
 import "./index.css";
@@ -47,6 +49,20 @@ function App() {
           2026, where I can apply my skills, evolve and contribute to projects!
         </p>
       </section>
+
+      <div className="flex flex-row gap-4">
+        <Social link="https://www.linkedin.com/in/lara-salman-00224b1b2/" ariaLabel="LinkedIn profile">
+          <Icon icon="ri:linkedin-fill" className="size-6" />
+        </Social>
+
+        <Social link="https://github.com/Laraass" ariaLabel="GitHub profile">
+          <Icon icon="mdi:github" className="size-6" />
+        </Social>
+
+        <Social link="" ariaLabel="My CV (Opens in new tab)">
+          <Icon icon="tabler:file-cv-filled" className="size-6" />
+        </Social>
+      </div>
 
       <section className="bg-shade-700 w-full min-h-screen py-28" id="about-me">
         {/* About Me */}
