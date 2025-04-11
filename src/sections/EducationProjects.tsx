81c6fcb58c4db12fad3ae20799ffc2da28d936b4
Lara <lara.salman01@hotmail.com>
2025-04-12 00:59:32 +0200

    fix deploy issues

---
+++ src/sections/EducationProjects.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EducationProjects: React.FC = () => {
+  return (
+    <section className="min-h-screen py-28" id="education-projects">
+      {/* Education & Projects */}
+    </section>
+  );
+};
+
+export default EducationProjects;
