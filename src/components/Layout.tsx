8aad54aebecad09ed8b188effce671d76490a918
Lara <lara.salman01@hotmail.com>
2025-04-12 05:00:34 +0200

    adjust layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: LayoutProps) => {
     <div className="flex flex-col items-center bg-shade-800 text-shade-100">
       <Header />
 
-      <main className="min-h-screen w-full flex-1 pb-20 pt-24 md:pb-16 md:pt-28">
+      <main className="min-h-screen w-full flex-1 pb-20 md:pb-16">
         {children}
       </main>
 
