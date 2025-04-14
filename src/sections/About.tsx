4e7ca9e8c68b0c36374c455bd540de15d699fd6d
Lara <lara.salman01@hotmail.com>
2025-04-14 05:20:28 +0200

    add content

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -10,18 +10,24 @@ const About: React.FC = () => {
     >
       <div className="flex flex-col gap-6 items-center">
         <h2 className="text-3xl font-bold text-primary">About me</h2>
-        <div className="flex flex-col md:flex-row gap-6 justify-center items-center">
+        <div className="flex flex-col md:flex-row gap-6 items-stretch">
           <Card
-            title="Title"
-            content="Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here"
+            title="Front-end"
+            content="I build interactive, user-friendly and responsive websites with a
+            focus on writing clean and maintainable code to enhance functionality and aesthetics. 
+            My approach ensures that websites are not only functional but also enjoyable to use."
           />
           <Card
-            title="Title"
-            content="Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here"
+            title="Back-end"
+            content="I build secure and robust backend systems, focusing on efficient APIs and 
+            database optimization. I handle authentication, data management, and ensure efficiency 
+            and security through clean, maintainable code and best practices."
           />
           <Card
-            title="Title"
-            content="Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here"
+            title="Teamwork"
+            content="I enjoy working with others and believe teamwork is important for success. 
+            I primarily use GitHub for version control and follow good project management methods. 
+            I adapt quickly to new challenges and help keep the team organized and efficient."
           />
         </div>
       </div>
