c3310266af53296effd05ff26b2ff1545195f4d7
Lara <lara.salman01@hotmail.com>
2025-04-14 05:11:45 +0200

    create basis for about me page

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import Card from "../components/Card";
 
 const About: React.FC = () => {
   return (
-    <section data-aos="fade-up" className="bg-shade-700 w-full min-h-screen py-28" id="about-me">
-      {/* About Me */}
+    <section
+      data-aos="fade-up"
+      className="max-w-7xl min-h-screen py-28 mx-auto"
+      id="about-me"
+    >
+      <div className="flex flex-col gap-6 items-center">
+        <h2 className="text-3xl font-bold text-primary">About me</h2>
+        <div className="flex flex-col md:flex-row gap-6 justify-center items-center">
+          <Card
+            title="Title"
+            content="Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here"
+          />
+          <Card
+            title="Title"
+            content="Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here"
+          />
+          <Card
+            title="Title"
+            content="Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here"
+          />
+        </div>
+      </div>
     </section>
   );
 };
