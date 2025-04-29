4c256043ce05cb83b62487dc9490df4124dd2e8f
Lara <lara.salman01@hotmail.com>
2025-04-29 20:47:12 +0200

    render contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import InputField from "../components/Inputfield";
+import Button from "../components/Button";
 
 const Contact: React.FC = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
-    <section data-aos="fade-up" className="bg-shade-800 min-h-screen py-28" id="contact">
-      {/* Contact */}
+    <section
+      data-aos="fade-up"
+      className="max-w-7xl min-h-screen py-28 mx-auto flex justify-center items-center"
+      id="contact"
+    >
+      <form 
+      className="flex flex-col gap-6 w-full max-w-lg items-center">
+        <h2 className="text-3xl font-bold text-primary">Contact me</h2>
+
+        <InputField
+          label="Full name"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <InputField
+          label="E-mail"
+          placeholder="Enter your e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <InputField
+          label="Phone number"
+          placeholder="Enter your phone number"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+
+        <InputField
+          label="Message"
+          placeholder="Write your message..."
+          type="textarea"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <Button text="Submit" onClick={() => alert("Message sent!")} />
+      </form>
     </section>
   );
 };
