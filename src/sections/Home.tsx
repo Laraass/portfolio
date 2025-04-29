ea15562328477200a6fefa15e8598f6d24b44591
Lara <lara.salman01@hotmail.com>
2025-04-29 23:31:35 +0200

    add CV

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -74,7 +74,7 @@ const Home: React.FC = () => {
               <Icon icon="mdi:github" className="size-6" />
             </Social>
 
-            <Social link="" ariaLabel="My CV (Opens in new tab)">
+            <Social link="/cv_sv_eng.pdf" ariaLabel="My CV (Opens in new tab)">
               <Icon icon="tabler:file-cv-filled" className="size-6" />
             </Social>
           </div>
